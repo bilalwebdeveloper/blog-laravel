32e67e0f03ceddd9244e3fd612e5f517097cd12c
Bilal Ahmad <bilal.webdeveloper433@gmail.com>
2024-10-11 23:05:36 +0500

    fix interface

--- app/Repositories/CategoryArticleRepositoryInterface.php
+++ app/Repositories/CategoryArticleRepositoryInterface.php
@@ -5,9 +5,9 @@ namespace App\Repositories;
 interface CategoryArticleRepositoryInterface
 {
     public function getCategoryArticles($categoryId, $offset, $limit);
+    public function getArticlesByCategoryId($categoryId);
     public function countArticlesByCategory($categoryId);
     public function getFirstArticleByCategoryId($categoryId);
-    public function getArticlesByCategoryId($categoryId);
     public function getLatestArticlesBySubCategories($subCategoryIds);
     
     
